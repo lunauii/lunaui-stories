ae4c6ee97c27578e08e7047586bc62d8c996b55a
Angelo Santos <angelo.rws@gmail.com>
2024-11-22 13:48:41 -0800

    Change default theme and modify Hero.svelte

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,9 +1,26 @@
+<script>
+    import 'boxicons';
+
+    function redirectToLunaui() {
+        window.location.href = 'https://lunaui.cc/';
+    }
+    
+    function redirectToGithub() {
+        window.location.href = 'https://github.com/lunauii/lunaui-stories/';
+    }
+</script>
+
 <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there</h1>
-        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-primary">Get Started</button>
+      <div class="max-w-xl">
+        <h1 class="text-5xl font-bold">503</h1>
+        <h2 class="text-3xl text-secondary">Service Unavailable</h2>
+        <p class="py-6">This page is currently under construction or maintenance! 
+            Please come back at a later time.</p>
+        <button class="btn btn-primary" onclick={redirectToLunaui}>Go Home</button>
+        <button class="btn btn-secondary btn-outline" onclick={redirectToGithub}>
+            Github<box-icon name='github' type='logo' color='white'></box-icon>
+        </button>
       </div>
     </div>
   </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,8 @@
 	import Hero from '../components/Hero.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+    <title>lunaui • stories 🦋</title>
+</svelte:head>
 
 <Hero />
