9049bc052c459dd5f2b03797b93cf9f72bb86d73
Angelo Santos <angelo.rws@gmail.com>
2024-11-22 12:27:16 -0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Hero from '../components/Hero.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Hero />
